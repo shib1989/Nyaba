ff1bd792c0b7948a2ba62c738b26e06f60227fc6
SPASM-project <github939@gmail.com>
2021-08-12 21:24:42 +0530

    initial commit

---
+++ app/src/main/java/com/topstack/nyaba/MainActivity.java
@@ -0,0 +1,135 @@
+package com.topstack.nyaba;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    NumberPicker picker1;
+    String[] pickerVals;
+    Button bookNowBtn;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Button bookingToInfo;
+
+    ImageView menuIcon;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bookNowBtn = findViewById(R.id.book_now_btn);
+        bookNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PersonalInfo.class);
+                startActivity(intent);
+            }
+        });
+
+        picker1 = findViewById(R.id.numberpicker_main_picker);
+        picker1.setMaxValue(4);
+        picker1.setMinValue(0);
+        pickerVals = new String[]{"1", "2", "3", "4", "5"};
+        picker1.setDisplayedValues(pickerVals);
+
+        picker1.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                int valuePicker1 = picker1.getValue();
+                Log.d("picker value", pickerVals[valuePicker1]);
+            }
+        });
+
+        menuIcon = findViewById(R.id.menu_icon);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        bookingToInfo = findViewById(R.id.book_to_info);
+        bookingToInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,BookingInfo.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+        navigationDrawer();
+
+
+    }
+
+    private void navigationDrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }else  drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_connect:
+
+                startActivity(new Intent(this,ContactInfo.class));
+                break;
+
+            case R.id.nav_feedback:
+                startActivity(new Intent(this, FeedBack.class));
+                break;
+
+            case R.id.nav_profile:
+                Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_payment:
+                Toast.makeText(this, "payment", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_log_out:
+                startActivity(new Intent(this, Login.class));
+                break;
+        }
+        return true;
+    }
+}
